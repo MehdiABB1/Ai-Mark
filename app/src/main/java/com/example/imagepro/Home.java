f8aa8d2ba48dde53c1583dd009f62daa73ac2a26
Mehdi Abbassi <mehdi.2000.abbassi@gmail.com>
2022-05-19 23:20:28 +0100

    Ai mark app

---
+++ app/src/main/java/com/example/imagepro/Home.java
@@ -0,0 +1,27 @@
+package com.example.imagepro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+    public void rectangle(View view) {
+        Intent intent = new Intent(this, Rectangle.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void camera(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
